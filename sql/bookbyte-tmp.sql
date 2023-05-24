8b540813e8ec846feb2de143c08a54f08cded356
chriskar02 <el20153@mail.ntua.gr>
2023-05-24 21:57:02 +0300

    3.1.1-3.1.4

--- sql/bookbyte-tmp.sql
+++ sql/bookbyte-tmp.sql
@@ -2,28 +2,45 @@
 
 -- find teacher loans
 create view teacher_loans as
-	select username, birth, isbn, category, first_name, last_name
+	select username, birth, isbn, category, name
 	from user natural join loan natural join book natural join teacher
-		natural join category
+		  natural join category
 ;
 
 -- 3.1.1
-select count(*) as total_loans
- from loan where in_out == 'borrowed'
- group by sch_id;
-
+create view school_loans as
+	select s.name as school_name, count(l.sch_id) as total_loans
+	from school s
+	join loan l on s.id = l.sch_id
+	where in_out = 'borrowed'  and date_format(l.date, '%Y-%m') = '2023-08'
+	group by s.name;
 -- 3.1.2
-select name from author natural join category where category == $user_input_category;
-
-select name
-from teacher_loans
-where category = $user_input_category;
+create view category_filter as
+	select distinct t.name as teacher_name, a.name as author_name
+	from teacher_loans t
+	join loan l on t.username = l.username
+	join category c on t.category = c.category
+	join author a on t.isbn = a.isbn
+	where datediff(now(), l.date) < 365 and c.category = 'History';
+	
 
 -- 3.1.3
-select first_name, last_name, sum(*) as n_loans
-from teacher_loans
-where datediff(now(),birth) < 14600
-group by username
-order by n_loans;
+create view youngster_loans as
+	select name, count(username) as n_loans
+	from teacher_loans
+	where datediff(now(),birth) < 14600
+	group by username
+	order by n_loans;
+
+
+--3.1.4
+create view no_loan as
+	select name as author
+	from author a
+	left join loan l on a.isbn = l.isbn
+	where l.isbn is null
+	group by author;
+
+--3.1.5
 
 
