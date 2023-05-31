3799df79c01e47cd0e51c28c5046ef1c3ebd892c
chriskar02 <el20153@mail.ntua.gr>
2023-05-31 08:43:24 +0300

    fixed admin5 for books lent instead of borrowed

--- app/templates/user_pages/admin5.php
+++ app/templates/user_pages/admin5.php
@@ -1,17 +1,22 @@
-<p>Handlers that have borrowed the same number of books, with more than 20 loans, within a one-year period.</p>
+<p>Handlers that have lent the same number of books, with more than 20 loans, within a one-year period.</p>
 <center>
 <?php
 
-$query = "SELECT l.username, COUNT(DISTINCT l.isbn) AS num_books_borrowed
-FROM loan AS l
-WHERE l.in_out = 'borrowed'
-  AND l.transaction_verified <> 0
-  AND l.date >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
-GROUP BY l.username
-HAVING COUNT(DISTINCT l.isbn) > 20;
+$query = "SELECT GROUP_CONCAT(handler_username), loaned
+FROM (
+	SELECT handler_username, COUNT(*) AS loaned
+	FROM loan
+	WHERE in_out = 'borrowed'
+		AND l.transaction_verified <> 0
+		AND l.date >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
+	GROUP BY handler_username
+	HAVING loaned > 20
+	) AS subquery
+GROUP BY loaned
+ORDER BY loaned DESC;
 ";
 $result = mysqli_query($conn, $query);
-echo '<table class="custom-table"><tr><thead><tr><th>Username</th><th>Number of Loans</th></tr></thead><tbody>';
+echo '<table class="custom-table"><tr><thead><tr><th>Handler Username</th><th>Number of Loans</th></tr></thead><tbody>';
 while($tr = mysqli_fetch_row($result)){
   echo '<tr><td>'. $tr[0] . '</td><td>'. $tr[1] . '</td></tr>';
 }
